a3471250a1ddcf9945578efcdb64476f5f04e0ee
闵超 <499790879@qq.com>
2023-05-23 14:58:17 +0800

    星级评分插件修改封装

---
+++ README.md
@@ -0,0 +1,93 @@
+## jquery 星星评级插件
+
+老项目焕发容光，为了应对苛刻的产品需求，要求在 C#的 web 项目中使用星星评级插件，且要求评级到小数点后一位
+
+不说了直接上图
+
+![效果图](./img/%E7%A4%BA%E4%BE%8B.png)
+
+### 参数及方法解释配置
+
+| 参数/方法 | 功能         | 初始值      |
+| --------- | ------------ | ----------- |
+| mode      | 评星模式     | LightEntire |
+| num       | 点亮初始值   | 0           |
+| readOnly  | 是否只读     | fasle       |
+| select    | 鼠标轻扫方法 | -           |
+| chosen    | 鼠标点击确定 | -           |
+
+### 使用示例
+
+// 引入 jquery 引入组件
+
+    <script type="text/javascript" src="http://libs.baidu.com/jquery/1.11.3/jquery.min.js"></script>
+    <script type="text/javascript" src="./jquery-starScoring.js"></script>
+
+// html 使用
+
+    <ul class="scoring">
+        <!-- i.rating-item[title]*5 -->
+        <li class="scoring-item"></li>
+        <li class="scoring-item"></li>
+        <li class="scoring-item"></li>
+        <li class="scoring-item"></li>
+        <li class="scoring-item"></li>
+    </ul>
+
+     <div>
+        打分结果: <span id="p"></span>
+    </div>
+
+// css 样式
+
+    * {
+        margin: 0px;
+        padding: 0px;
+    }
+
+    li {
+        list-style: none;
+    }
+
+    .scoring {
+        width: 180px;
+        height: 25px;
+        margin: 20px auto;
+        overflow-x: 0;
+    }
+    .scoring .scoring-item{
+        height: 25px;
+        width: 25px;
+        margin:0 5px;
+        background: url(./img/star.png) no-repeat;
+        /* background-position: 0 -50px; */
+        cursor: pointer;
+        float: left;
+    }
+    div{
+        text-align: center;
+    }
+
+// 初始化插件
+
+     starScoring.init('.scoring', {
+            mode: 'LightEntire', //  LightEntire/LightHalf/LightTen
+            num: 2, //点亮初始值
+        });
+        $('.scoring').on('select', function(e, num, total) {
+            $('#p').html(num)
+        }).on('chosen', function(e, num, total) {
+            $('#p').html(num)
+        })
+
+解释说明：其中 mode 有三种模式可供选择，分别是：
+
+    LightEntire 整颗星点亮
+    LightHalf   半颗星点亮
+    LightTen    十分之一颗星点亮
+
+## 结束语
+
+最后附上 img 的背景图，以及源 psd
+
+对您有用的话，记得给个 star 点个赞
---
+++ jquery-starScoring.js
@@ -0,0 +1,175 @@
+var starScoring = (function() {
+    //继承
+    var extend = function(subClass, superClass) {
+        var F = function() {};
+        F.prototype = superClass.prototype;
+        subClass.prototype = new F();
+        subClass.prototype.constructor = subClass;
+    }
+
+
+
+    //抽象类点亮
+    var Light = function(el, options) {
+        this.el = $(el);
+        this.item = this.el.find('.scoring-item');
+        this.opts = options;
+        this.add = 0;
+        this.selectEvent = "mouseover";
+    };
+    Light.prototype.init = function() {
+        this.lightOn(this.opts.num);
+        if (!this.opts.readOnly) {
+            this.bindEvent();
+        }
+
+    };
+    Light.prototype.lightOn = function(num) {
+        num = parseInt(num); //转化为整型
+        this.item.each(function(index) {
+            if (index >= num) {
+                $(this).css('background-position', '0 -250px');
+            } else {
+                $(this).css('background-position', '0 0');
+            }
+        });
+    };
+    //事件
+    Light.prototype.bindEvent = function() {
+        //事件绑定
+        var self = this;
+        var itemLength = this.item.length;
+        this.el.on(self.selectEvent, '.scoring-item', function(e) {
+            var _num = 0;
+            var $this = $(this);
+
+            self.select(e, $this);
+            _num = $(this).index() + self.add;
+            self.lightOn(_num);
+            self.el.trigger('select', [_num, itemLength]);
+        }).on('click', '.scoring-item', function(e) {
+            self.chosen(e, $(this));
+            self.opts.num = $(this).index() + self.add;
+            self.lightOn(self.opts.num);
+            self.el.trigger('chosen', [self.opts.num, itemLength]);
+        }).on('mouseout', function() {
+            self.lightOn(self.opts.num);
+        })
+    }
+    Light.prototype.select = function() {
+        throw new Error("子类必须重写此方法");
+    };
+    Light.prototype.chosen = function() {
+        throw new Error("子类必须重写此方法");
+    };
+
+    //点亮整颗
+    var LightEntire = function(el, options) {
+        Light.call(this, el, options) //继承父类构造函数内容
+        this.selectEvent = "mouseover";
+    };
+    extend(LightEntire, Light);
+
+    LightEntire.prototype.lightOn = function(num) {
+        Light.prototype.lightOn.call(this, num);
+    };
+    LightEntire.prototype.select = function() {
+        this.add = 1;
+    };
+    LightEntire.prototype.chosen = function() {
+        this.add = 1;
+    };
+
+    // 点亮半颗
+    var LightHalf = function(el, options) {
+        Light.call(this, el, options)
+        this.selectEvent = "mousemove";
+    };
+    extend(LightHalf, Light);
+    LightHalf.prototype.lightOn = function(num) {
+        var count = parseInt(num);
+        var isHalf = num !== count;
+        Light.prototype.lightOn.call(this, count);
+        if (isHalf) {
+            this.item.eq(count).css('background-position', '0 -125px'); //eq(n)方法，选择第n个元素,从0开始
+        }
+
+    };
+
+    LightHalf.prototype.select = function(e, $this) {
+        if (e.pageX - $this.offset().left < $this.width() / 2) {
+            this.add = 0.5;
+        } else if(e.pageX - $this.offset().left > $this.width() / 2){
+            this.add = 1;
+        }else{
+            this.add = 0;
+        }
+    };
+    LightHalf.prototype.chosen = function(e, $this) {
+        if (e.pageX - $this.offset().left < $this.width() / 2) {
+            this.add = 0.5;
+        } else if(e.pageX - $this.offset().left > $this.width() / 2){
+            this.add = 1;
+        }else{
+            this.add = 0;
+        }
+    };
+
+    // 点亮十分之一颗
+    var LightTen = function(el, options) {
+        Light.call(this, el, options)
+        this.selectEvent = "mousemove";
+    };
+    extend(LightTen, Light);
+    LightTen.prototype.lightOn = function(num) {
+      var count = parseInt(num);
+ 
+      Light.prototype.lightOn.call(this, count);
+      var percentage = Math.floor((num - count) * 10);
+      this.item.eq(count).css('background-position', '0 -'+ 25*(10-percentage)+'px');
+      
+    };
+
+    LightTen.prototype.select = function(e, $this) {
+      var positionX = e.pageX - $this.offset().left;
+      var itemWidth = $this.width() / 10;
+      var selectedFraction = parseFloat((Math.round(positionX / itemWidth) * 0.1).toFixed(1));
+      this.add = selectedFraction;
+    };
+    LightTen.prototype.chosen = function(e, $this) {
+      var positionX = e.pageX - $this.offset().left;
+      var itemWidth = $this.width() / 10;
+      var selectedFraction = parseFloat((Math.round(positionX / itemWidth) * 0.1).toFixed(1));
+      this.add = selectedFraction;
+    };
+
+
+    //默认参数
+    var defaults = {
+        mode: 'LightEntire',
+        num: 0,
+        readOnly: false,
+        select: function() {},
+        chosen: function() {},
+    }
+
+    var mode = {
+        'LightEntire': LightEntire,
+        'LightHalf': LightHalf,
+        'LightTen': LightTen,
+
+    };
+
+    //init
+    var init = function(el, options) {
+        options = $.extend({}, defaults, options); //初始化，默认值defaluts,如果option设置参数，则覆盖默认值
+        if (!mode[options.mode]) {
+            mode[options.mode] = "LightEntire";
+        }
+        new mode[options.mode](el, options).init();
+    }
+    return {
+        init: init
+    };
+
+})();
